24150918ca92543384c91ff2c63c9281912235d8
joachim chikezie <56943504+chuddyjoachim@users.noreply.github.com>
2021-06-12 14:04:27 -0700

    Added shorten_link api routes

---
+++ pages/api/shorten_link.ts
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { connectDb } from "../../util/mongodbConfig";
+import { nanoid } from "nanoid";
+
+type shortid = {};
+
+export default async (req: NextApiRequest, res: NextApiResponse<shortid>) => {
+  if (req.method === "POST") {
+    const { db } = await connectDb();
+
+    if (
+      req.body !== "" &&
+      req.body.link !== undefined &&
+      req.body.link !== ""
+    ) {
+      let body = JSON.parse(req.body).link;
+      const id_ = nanoid();
+
+      const entry = await db
+        .collection("url")
+        .insertOne({ shortid: id_, url: body });
+      const reponse = await db.collection("url").findOne({ url: body });
+
+      res.statusCode = 201;
+      return res.json({ shortid: reponse.shortid });
+    }
+
+    res.statusCode = 409;
+    res.json({ error: "no_link_found", error_description: "No link found" });
+  }
+};
