efdcb3253c5a3cf847a8255084c6d349f8eaff89
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2021-09-20 14:25:26 +0100

    Add and configure fonts with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        headline: ['Righteous-Regular']
+      }
+    },
   },
   variants: {
     extend: {},
